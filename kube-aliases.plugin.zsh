e61dcd6260c46850f5cf21b13e4a76d6526e049e
Alan Jenkins <a.jenkins@synalogik.com>
2021-05-20 09:53:19 +0100

    Fix issues with slowness and paths
    Kubectl's completion takes ages to load when loaded as part of this
    plugin. Removing the loading of the completion from the inside of the
    plugin and doing it outside with the help of zinit (so it can defer the
    loading and speed things up).
    
    Also fixing issues with the plugin paths when used with zinit as it does
    not download the plugin with the directory name of kube-aliases.

--- kube-aliases.plugin.zsh
+++ kube-aliases.plugin.zsh
@@ -1,10 +1,7 @@
-KALIAS=$ZSH_CUSTOM/plugins/kube-aliases
-KRESOURCES=$ZSH_CUSTOM/plugins/kube-aliases/docs/resources
-SHELL_NAME=$(basename $SHELL)
-
-# Auto complete, for bash replace zsh with bash
-# For some reason this is sourcing the oh-my-zsh plugin.
-source <(kubectl completion $SHELL_NAME)
+PLUGIN_DIR="$(dirname $0:A)"
+KALIAS="$PLUGIN_DIR"
+KRESOURCES="$PLUGIN_DIR/kube-aliases/docs/resources"
+SHELL_NAME="$(basename $SHELL)"
 
 # Contexts
 alias kcc='kubectl config get-contexts'
